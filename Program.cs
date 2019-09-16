8b4ada248b96770ed26d47fdc8203107296fa4d8
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-16 11:01:11 -0400

    Add classes and complete project.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace StringIndexer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Indexers *****\n");
+
+            PersonCollection myPeople = new PersonCollection();
+
+            myPeople["Homer"] = new Person("Homer", "Simpson", 40);
+            myPeople["Marge"] = new Person("Marge", "Simpson", 38);
+
+            // Get "Homer" and print data.
+            Person homer = myPeople["Homer"];
+            Console.WriteLine(homer.ToString());
+
+            Console.ReadLine();
         }
     }
 }
